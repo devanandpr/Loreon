768f4d700d44a3c5e0b86534472c468d3f6eb716
devanandpr <devanand0903@gmail.com>
2026-08-28 23:37:16 +0530

    Added API URL

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
 
 /* =====================================================
    PRODUCTS
